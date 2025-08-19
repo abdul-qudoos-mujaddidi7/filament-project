bdaea541a37e59eedce05e95ed895aeb9dc100b3
Jawad Mujaddidi <abdulqudoosmujaddidi7@gmail.com>
2025-08-19 17:12:13 +0430

    Dashboard

--- app/Filament/App/Widgets/StatsAppOverview.php
+++ app/Filament/App/Widgets/StatsAppOverview.php
@@ -2,6 +2,11 @@
 
 namespace App\Filament\App\Widgets;
 
+use App\Models\Deparmtment;
+use App\Models\Employee;
+use App\Models\Team;
+use App\Models\User;
+use Filament\Facades\Filament;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +15,20 @@ class StatsAppOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Users', Team::find(Filament::getTenant())->members()->count())
+            ->description('All users from the database')
+            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+        Stat::make('Department', Deparmtment::query()->whereBelongsTo(Filament::getTenant())->count())
+            ->description('All teams from the database')
+            ->descriptionIcon('heroicon-m-arrow-trending-down'),
+        Stat::make('Employees', Employee::count())
+            ->description('All Employess from the database')
+            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+            Stat::make('Unique views', '192.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
+            ->color('success'),
         ];
     }
 }
---
+++ app/Filament/Widgets/LatestEmployees.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Employee;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestEmployees extends BaseWidget
+{
+    protected int | string | array $columnSpan = 'full';
+    protected static ?int $sort = 4;
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Employee::query())
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                TextColumn::make('count.name'),
+                TextColumn::make('first_name'),
+                TextColumn::make('last_name')
+            ]);
+    }
+}
